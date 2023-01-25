b6c1edb8d66ad8ca877c6953ea3e11a5caee10d5
Ek Ae <ekzin15@gmail.com>
2023-01-25 10:29:54 +0700

    name

--- controllers/products.js
+++ controllers/products.js
@@ -9,7 +9,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company } = req.query;
+  const { featured, company, name } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -18,6 +18,10 @@ const getAllProducts = async (req, res) => {
 
   if (company) {
     queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
   }
 
   const products = await Product.find(queryObject);
